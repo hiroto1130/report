908e2baf9987cf24db67db44646eeac406843f2e
mono1140 <11potato30@gmail.com>
2019-07-11 19:51:28 +0900

    std の乱数に関するcodeの追加

---
+++ std_rund/Source.cpp
@@ -0,0 +1,14 @@
+﻿#include <random>
+#include <iostream>
+
+int main()
+{
+	std::mt19937 mt{ std::random_device{}()};
+
+	std::uniform_int_distribution<int> a(1, 100);
+
+	for (int i = 0; i != 10; ++i)
+	{
+		std::cout << a(mt) << std::endl;
+	}
+}
