d931552ee7f78df0adcfa92f27a13700bf50da61
mono1140 <11potato30@gmail.com>
2019-07-23 09:59:09 +0900

    :art: std::vector に関するコードの追加2

--- std_rund/Source.cpp
+++ std_rund/Source.cpp
@@ -5,10 +5,12 @@ int main()
 {
 	std::mt19937 mt{ std::random_device{}()};
 
-	std::uniform_int_distribution<int> a(1, 100);
+	std::uniform_int_distribution<int> a(1, 6);
 
 	for (int i = 0; i != 10; ++i)
 	{
+		
 		std::cout << a(mt) << std::endl;
+
 	}
 }
---
+++ std_vector/Source.cpp
@@ -0,0 +1,22 @@
+﻿#include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <random>
+#include <iostream>
+
+
+int main()
+{
+	std::mt19937 mt{ std::random_device{}() };
+	std::uniform_int_distribution<int> a(1, 6);
+	std::uniform_int_distribution<int> e(1, 10);
+	std::vector<int> vi(10);
+
+	// 代入
+	for (int i = 0; i < 10; i++)
+	{
+		vi[i] = a(mt);
+	}
+
+	std::cout << vi[e(mt)] << std::endl;
+}
